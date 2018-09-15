080910d7136b7fd5c6542402d8d15b8b15410c6d
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-15 04:37:16 +0300

    Brew formula update for gosling version v0.1.1

--- Formula/gosling.rb
+++ Formula/gosling.rb
@@ -1,9 +1,9 @@
 class Gosling < Formula
   desc "HTML redirects generator"
   homepage "https://github.com/leonidboykov/gosling"
-  url "https://github.com/leonidboykov/gosling/releases/download/v0.1.0/gosling_0.1.0_macOS_x86_64.tar.gz"
-  version "0.1.0"
-  sha256 "0f2ca80742ab4fe4f23a02aabb341cc931490635645773e37a9513ddb64c8dae"
+  url "https://github.com/leonidboykov/gosling/releases/download/v0.1.1/gosling_0.1.1_macOS_x86_64.tar.gz"
+  version "0.1.1"
+  sha256 "8bbe018e0797327594005c580a56a06ad1b4f97e5b580eb1fd7e4469ea7706fc"
 
   def install
     bin.install "gosling"
