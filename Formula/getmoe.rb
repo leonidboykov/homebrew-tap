4cbb3f5f0fb1e039d73d63a63ff67cc0869566c9
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-04 21:35:08 +0300

    Brew formula update for getmoe version v0.3.4

--- Formula/getmoe.rb
+++ Formula/getmoe.rb
@@ -1,9 +1,9 @@
 class Getmoe < Formula
   desc "cli tool for boorus"
   homepage "https://github.com/leonidboykov/getmoe"
-  url "https://github.com/leonidboykov/getmoe/releases/download/v0.3.3/getmoe_0.3.3_macOS_x86_64.tar.gz"
-  version "0.3.3"
-  sha256 "b4cf7af0be6b267a0e4348de24b397e179681139c093f864857fb47a9788b514"
+  url "https://github.com/leonidboykov/getmoe/releases/download/v0.3.4/getmoe_0.3.4_macOS_x86_64.tar.gz"
+  version "0.3.4"
+  sha256 "82b1e0dad809a85cffd389ca33a3b6548b91038d89d1d0a8b5880599530d8073"
 
   def install
     bin.install "getmoe"
